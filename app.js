633c24cf29aeb1750282841fcca4a96dd4dda1b0
geng-liu <geng.liu@stonybrook.edu>
2016-03-30 10:27:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 console.log('app.js loaded')
 
-document.body.innerHTML+=' Please input <input id="val" size=4 value="20">'
+document.body.innerHTML+=' Please input <input id="field1" size=4 value="20">'
+
+field1.onkeyup=function(evt){
+  document.body.innerHTML += "\n You have input " + field1.value
+}
 console.log('hello world')
