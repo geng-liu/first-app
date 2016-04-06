55bc271b99b1ecf39ab5b745a5a62eac0f3d40da
geng-liu <geng.liu@stonybrook.edu>
2016-04-06 13:52:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 console.log('app.js loaded')
 
-document.body.innerHTML+=' Please input <input id="field1" size=4>'
+//document.body.innerHTML+=' Please input <input id="field1" size=4>'
 
-field1.onkeyup=function(evt){
-  document.body.innerHTML += "\n You have input " + field1.value
-}
+//field1.onkeyup=function(evt){
+//  document.body.innerHTML += "\n You have input " + field1.value
+//}
+
+var json_obj = JSON.parse("http://localhost:8500/rest/RestTest/courseService/7659.json");
+
+document.body.innerHTML = json_obj.columns[0].FACID;
+alert(json_obj.count);
+
 console.log('hello world')
